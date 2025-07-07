e55fa9ebbdf9e29867d9f0971d6b7b821f1b97c8
beyzanuruzunkaya <beyzanuruzunkaya04@gmail.com>
2025-07-07 14:15:47 +0300

    Add project files.

---
+++ MiniE-TicaretPaneli/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniE_TicaretPaneli.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
